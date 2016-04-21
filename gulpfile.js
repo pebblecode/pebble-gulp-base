e2ca0ed2de6c1601026b1105ee6b707722c865f0
Mark Durrant <mark.j.durrant.gmail.com>
2016-04-21 16:09:45 +0100

    start on fixing npm issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,40 @@
 "use strict";
 
-// Plugins
-var            gulp = require('gulp'),
-            connect = require('connect'),
-  connectLivereload = require('connect-livereload'),
-     gulpLivereload = require('gulp-livereload'),
-               sass = require('gulp-sass'),
-             prefix = require('gulp-autoprefixer'),
-             jshint = require('gulp-jshint'),
-           sequence = require('gulp-sequence'),
-             rimraf = require('rimraf'),
-            stylish = require('jshint-stylish');
+// plugins
+var gulp = require('gulp'),
+    connect = require('connect'),
+    serveStatic = require('serve-static'),
+    connectLivereload = require('connect-livereload'),
+    sass = require('gulp-sass'),
+    prefix = require('gulp-autoprefixer');
 
-// paths & files
 var path = {
-        src: 'src/',
-       html: 'src/**/*.html',
-         js: 'src/js/*.js',
-       sass: 'src/sass/**/*.scss',
-        css: 'src/css/',
-};
+   src: 'src/',
+  html: 'src/**/*.html',
+    js: 'src/js/*.js',
+  sass: 'src/sass/**/*.scss',
+   css: 'src/css/',
+}
 
-// ports
-var localPort =  4000,
+var localPort = 4000,
        lrPort = 35729;
 
-// start local server
-gulp.task( 'server', function() {
+gulp.task('server', function(){
   var server = connect();
 
-  server.use( connectLivereload( { port: lrPort } ) );
-  server.use( connect.static( path.src ) );
-  server.listen( localPort );
+  server.use(connectLivereload({port: lrPort}));
+  server.use(serveStatic(path.src));
+  server.listen(localPort);
 
-  console.log( "\nlocal server running at http://localhost:" + localPort + "/\n" );
+  console.log("\nlocal server running at http://localhost:" + localPort + "/\n");
 });
 
-// jshint
-gulp.task( 'jshint', function() {
-  gulp.src( path.js )
-    .pipe( jshint() )
-    .pipe( jshint.reporter( stylish ) );
-});
-
-// compile sass
-gulp.task( 'sass', function() {
-  gulp.src( path.sass )
-    .pipe(sass().on('error', sass.logError))
-    .pipe( sass({
+gulp.task('sass', function(){
+  gulp.src(path.sass)
+    .pipe(sass({
       outputStyle: [ 'expanded' ],
       sourceComments: 'normal'
-    }))
-    .pipe( prefix() )
-    .pipe( gulp.dest( path.css ) );
-});
-
-gulp.task('sass:build', function() {
-  gulp.src( path.sass )
-  .pipe( sass({
-    outputStyle:'compressed'
-  }))
-  .pipe( prefix() )
-  .pipe( gulp.dest( path.css ) );
-});
-
-// watch file
-gulp.task( 'watch', function(done) {
-  var lrServer = gulpLivereload();
-
-  gulp.watch( [ path.html, path.js, path.css + '/**/*.css' ] )
-    .on( 'change', function( file ) {
-      lrServer.changed( file.path );
-    });
-
-  gulp.watch( path.js, ['jshint'] );
-
-  gulp.watch( path.sass, ['sass'] );
-});
-
-// default task
-gulp.task( 'default', [ 'server', 'watch' ] );
-
-gulp.task('copy:build', function() {
-  gulp.src([
-    './src/css/**/*.css',
-    './src/**/*.html',
-    './src/js/**/*.js',
-    './src/fonts/**/*.*',
-    './src/doc/**/*.*',
-    './src/img/**/*.*',
-    './src/favicon.ico'
-  ], { base: './src' })
-  .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function(cb) {
-  rimraf('./dist', cb);
+    }).on('error', sass.logError))
+    .pipe(prefix())
+    .pipe(gulp.dest(path.css));
 })
-
-gulp.task('build', function(cb) {
-  sequence('clean', 'sass:build', 'copy:build')(cb);
-});
